4a2d70b6f0356d0f1ecbbafa125b9482b395d2ce
Daksh Jain <dajain@ucdavis.edu>
2023-11-19 14:42:37 -0800

    fixed linechart not showing up

--- components/line-chart.tsx
+++ components/line-chart.tsx
@@ -14,10 +14,15 @@ interface LineChartProps {
 
 const LineChartComponent:  React.FC<LineChartProps> = ({ data }: LineChartProps) => {
     return (
-        <div className='flex flex-col h-100 shadow-lg rounded-lg'>
+        <div className='flex flex-col h-screen shadow-lg rounded-lg'>
             <div className='flex items-center bg-gradient-to-r from-cyan-500 to-blue-500 text-black text-xl py-1 font-bold'>Chart Data</div>
             <div className='flex flex-row p-2 justify-between bg-white'>
-                <ResponsiveContainer width="100%" height="60%">
+                <div className='flex flex-col justify-center h-[80%] w-[20%] m-[2%]'>
+                    <div className='flex text-black justify-center text-xl font-bold'>Insights</div>
+                    <div className='flex text-black justify-center text-center text-lg'>Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  </div>
+                    <div className='mx-[10%] flex bg-slate-300 justify-center text-black text-lg rounded-full hover:bg-slate-500 text-center'>Details</div>                
+                </div>
+                <ResponsiveContainer width="100%" height="100%">
                 <LineChart data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
@@ -30,11 +35,6 @@ const LineChartComponent:  React.FC<LineChartProps> = ({ data }: LineChartProps)
                     <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                 </LineChart>
                 </ResponsiveContainer>
-                <div className='flex flex-col ml-[10%] h-[80%] w-[20%] my-[10%]'>
-                    <div className='flex text-black justify-center text-xl font-bold'>Insights</div>
-                    <div className='flex text-black justify-center text-center text-lg'>Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  Lorem  </div>
-                    <div className='mx-[10%] flex bg-slate-300 justify-center text-black text-lg rounded-full hover:bg-slate-500 text-center'>Details</div>                
-                </div>
             </div>
         </div>
     );
