602d3540a9c9390791ab621db3c9bcb408fe92e0
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2023-12-17 23:01:30 -0800

    Add homepage screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,39 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+    <div
+      style={{
+        backgroundImage: 'url("bg.svg")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '93vh',
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div className='text-black text-2xl font-bold flex flex-row'>
+        <a href="/admin">
+          <div className='bg-grad3-start px-10 py-8 flex flex-col justify-center rounded-lg hover:scale-105 duration-100'>
+            <h1>Admins</h1>
+            <h2>Log In here</h2>
+          </div>
+        </a>
+        <a href="/employer">
+          <div className='bg-grad3-end px-10 py-8 ml-4 flex flex-col justify-center rounded-lg hover:scale-105 duration-100'>
+            <h1>Employers</h1>
+            <h2>Log In here</h2>
+          </div>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+
+function Home2() {
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,18 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function Header() {
-
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    useEffect(() => {
-        setIsMenuOpen(false);
-    }, []);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     return (
-        <div className="bg-slate-600 w-full">
+        <div className="bg-slate-600 w-full h-[7vh]">
             <div className="container mx-auto flex justify-between items-center">
-                <Image src="/logo.png" alt="Billion Minds Logo" className="w-16 h-auto" width={1080} height={1080} />
+                <a href="/">
+                    <Image src="/logo.png" alt="Billion Minds Logo" className="w-16 h-auto" width={1080} height={1080} />
+                </a>
                 <div className="relative">
                     <button onClick={toggleMenu} className="flex flex-col justify-around w-6 h-4">
                         <div className="w-full h-0.5 bg-white"></div>
